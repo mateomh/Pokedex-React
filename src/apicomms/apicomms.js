7c084e4f91b15aa7f39f10d898c699b28051be4b
mateomh <mmojicah.84@gmail.com>
2020-11-28 19:07:40 -0500

    Refactor the API calls into a separate method getDataFromApi

--- src/apicomms/apicomms.js
+++ src/apicomms/apicomms.js
@@ -1,19 +1,22 @@
 const endpointUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
+const getDataFromApi = async url => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
 const getPokemons = async (page = 1, numberperpage = 10) => {
   const paginationparams = `?limit=${numberperpage}&offset=${(page - 1) * numberperpage}`;
   const pokemonUrl = endpointUrl.concat(paginationparams);
 
-  const data = await fetch(pokemonUrl);
-  const pokedata = await data.json();
+  const pokedata = getDataFromApi(pokemonUrl);
   return pokedata;
 };
 
 const getOnePokemonInfo = async pokemon => {
   const pokemonUrl = endpointUrl.concat(pokemon);
-
-  const data = await fetch(pokemonUrl);
-  const pokedata = await data.json();
+  const pokedata = getDataFromApi(pokemonUrl);
   return pokedata;
 };
 
