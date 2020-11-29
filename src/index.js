80ae0aa14865cbedd228e871b344983466b6cb08
mateomh <mmojicah.84@gmail.com>
2020-11-28 21:00:39 -0500

    Add router to the project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import Routes from './routes';
 
 ReactDOM.render(
-  <>
-    <App />
-  </>,
+  <React.StrictMode>
+    <Routes />
+  </React.StrictMode>,
   document.getElementById('root'),
 );
