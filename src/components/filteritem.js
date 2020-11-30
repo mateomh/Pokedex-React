842d74d8c4d0681791e06a347737f3c0c14baf77
mateomh <mmojicah.84@gmail.com>
2020-11-30 11:08:07 -0500

    Add click function to the filteritem component

--- src/components/filteritem.js
+++ src/components/filteritem.js
@@ -1,11 +1,20 @@
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import PropTypes from 'prop-types';
 import TypeBadge from './typebadge';
 import FilterItemStyles from './styles/filteritem.module.css';
 
 const FilterItem = props => {
   const { type } = props;
+
+  const filterClick = event => {
+    const { target: filter } = event;
+    console.log(filter.id);
+    filter.classList.toggle(FilterItemStyles.Selected);
+  };
+
   return (
-    <div className={FilterItemStyles.Item}>
+    <div className={FilterItemStyles.Item} onClick={filterClick} role="button" id={type}>
       <TypeBadge type={type} />
     </div>
   );
