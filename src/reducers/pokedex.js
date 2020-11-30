bc49074586957bc4726c34534cb139f165416938
mateomh <mmojicah.84@gmail.com>
2020-11-29 20:01:01 -0500

    Add check in the pokedex reducer to not add repeated data

--- src/reducers/pokedex.js
+++ src/reducers/pokedex.js
@@ -4,6 +4,9 @@ const pokedexReducer = (state = defaultstate, action) => {
   const { type, payload: pokemon } = action;
   switch (type) {
     case 'ADD_POKEMON':
+      if (state.find(element => element.name === pokemon.name)) {
+        return state;
+      }
       return state.concat(pokemon);
     default:
       return state;
