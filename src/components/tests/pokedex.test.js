3c8bbdde85f09297c01d02934caa0901e2baee0b
mateomh <mmojicah.84@gmail.com>
2020-12-01 19:13:05 -0500

    Add pokedex component tests

---
+++ src/components/tests/pokedex.test.js
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../../reducers/root';
+import PokeDex from '../pokedex';
+
+describe('PokeDex Component', () => {
+  beforeEach(() => {
+    const pokemons = [
+      {
+        name: 'Defaulty',
+        imagesm: '',
+        types: ['1', '2'],
+        id: 5000,
+      },
+    ];
+
+    const mockState = {
+      filter: 'All',
+      pokemons,
+      page: 50,
+      currentPokemon: 'Defaulty',
+    };
+
+    const mockStore = createStore(rootReducer, mockState);
+    render(
+      <Provider store={mockStore}>
+        <PokeDex />
+      </Provider>,
+    );
+  });
+
+  it(
+    '1. Renders correctly',
+    () => {
+      const PokeName = screen.getByText('Defaulty');
+      expect(PokeName).toBeDefined();
+    },
+  );
+});
