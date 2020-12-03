ba798244649498413d0757889ae8469564cfd9c8
mateomh <mmojicah.84@gmail.com>
2020-12-03 08:48:01 -0500

    Fix linter errors

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import PokeDex from './components/pokedex';
-import PokeInfo from './components/pokeinfo';
+import PokeDex from './containers/pokedex';
+import PokeInfo from './containers/pokeinfo';
 
 const Routes = () => (
   <BrowserRouter>
